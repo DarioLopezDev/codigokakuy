fa11f3ec6a8a21585c86a70453b425a4b4312ecf
Dario Lopez <dariolopezdeveloper@gmail.com>
2023-11-28 22:10:11 -0300

    creando rutas de acceso

--- src/routes/mainRouter.js
+++ src/routes/mainRouter.js
@@ -3,5 +3,10 @@ const router = express.Router();
 
 const controlador = require('../controllers/mainController');
 
+router.get('/', controlador.index);
+router.get('/carrito', controlador.productCart);
+router.get('/detalles', controlador.productDetail);
+router.get('/login', controlador.login);
+router.get('/register', controlador.register);
 
 module.exports = router;
