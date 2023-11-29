387382fabd333a496835443da064eaf82d9aa66d
SantiagoAbdala <santiagoabdalabriz@gmail.com>
2023-11-28 21:08:35 -0300

    empezando sprint 3
    creando carpetas y cambiando archivos ejs

--- app.js
+++ app.js
@@ -8,27 +8,27 @@ const { CLIENT_RENEG_LIMIT } = require('tls');
 const publicPath = path.join(__dirname, 'public');
 app.use(express.static(publicPath));
 
-const carritoPath = path.join(__dirname, 'views/carrito.html');
+const carritoPath = path.join(__dirname, '/src/views/carrito.ejs');
 app.get('/carrito', (req, res) => {
     res.sendFile(carritoPath);
 });
 
-const detallesPath = path.join(__dirname, 'views/detalles.html');
+const detallesPath = path.join(__dirname, '/src/views/detalles.ejs');
 app.get('/detalles', (req, res) => {
     res.sendFile(detallesPath);
 });
 
-const homePath = path.join(__dirname, 'views/home.html');
+const homePath = path.join(__dirname, '/src/views/home.ejs');
 app.get('/', (req, res) => {
     res.sendFile(homePath);
 });
 
-const loginPath = path.join(__dirname, 'views/login.html');
+const loginPath = path.join(__dirname, '/src/views/login.ejs');
 app.get('/login', (req, res) => {
     res.sendFile(loginPath);
 });
 
-const registerPath = path.join(__dirname, 'views/register.html');
+const registerPath = path.join(__dirname, '/src/views/register.ejs');
 app.get('/register', (req, res) => {
     res.sendFile(registerPath);
 });
