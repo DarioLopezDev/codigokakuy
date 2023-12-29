1bf41bbb74200a3a0235a13e708e51e47e176672
SantiagoAbdala <santiagoabdalabriz@gmail.com>
2023-12-28 22:00:26 -0300

    cambios al css y ejs

--- app.js
+++ app.js
@@ -26,3 +26,73 @@ app.listen(port, () => {
     http://localhost:4050/
     `);
 });
+
+
+/*
+function filtrarLibros() {
+    const name = document.getElementById("name").value;
+    const año = parseInt(document.getElementById("año").value);
+    const autor = document.getElementById("autor").value;
+    const genero = document.getElementById("genero").value;
+    const price = parseFloat(document.getElementById("price").value);
+    const editorial = document.getElementById("editorial").value;
+    const ISBN = document.getElementById("ISBN").value;
+
+    const filtro = {};
+
+    if (name !== "") {
+        filtro["name"] = name;
+    }
+
+    if (!isNaN(año)) {
+        filtro["año"] = año;
+    }
+
+    if (autor !== "") {
+        filtro["autor"] = autor;
+    }
+
+    if (genero !== "") {
+        filtro["genero"] = genero;
+    }
+
+    if (!isNaN(price)) {
+        filtro["price"] = price;
+    }
+
+    if (editorial !== "") {
+        filtro["editorial"] = editorial;
+    }
+
+    if (ISBN !== "") {
+        filtro["ISBN"] = ISBN;
+    }
+
+    const librosFiltrados = libros.filter(libro => {
+        for (const clave in filtro) {
+            if (libro[clave] !== filtro[clave]) {
+                return false;
+            }
+        }
+        return true;
+    });
+
+    mostrarResultados(librosFiltrados);
+}
+
+function mostrarResultados(resultados) {
+    const resultadosDiv = document.getElementById("resultados");
+    resultadosDiv.innerHTML = "";
+
+    if (resultados.length === 0) {
+        resultadosDiv.innerHTML = "No se encontraron resultados.";
+        return;
+    }
+
+    resultados.forEach(libro => {
+        const libroDiv = document.createElement("div");
+        libroDiv.innerHTML = `<strong>${libro.name}</strong> - Autor: ${libro.autor}, Género: ${libro.genero}, Precio: ${libro.price}`;
+        resultadosDiv.appendChild(libroDiv);
+    });
+}
+*/
