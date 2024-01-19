249d3298614013207cc13e1f3ea94ba7fc76a569
Nicolas-Vettorazzo <nicolasvettorazzo@gmail.com>
2024-01-19 17:59:21 -0300

    Modifico usersRouter

--- src/routes/mainRouter.js
+++ src/routes/mainRouter.js
@@ -4,7 +4,6 @@ const router = express.Router();
 const controlador = require('../controllers/mainController');
 
 router.get('/', controlador.index);
-router.get('/login', controlador.login);
-router.get('/register', controlador.register);
+
 
 module.exports = router;
--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -3,4 +3,21 @@ const router = express.Router();
 const path = require('path');
 const multer = require('multer');
 
-module.exports = {}
+const usersController = require('../controllers/usersController');
+
+let storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, path.resolve(__dirname, '../../public/images/users'))
+    },
+    filename: function (req, file, cb) {
+        let fileName = `${Date.now()}_img${path.extname(file.originalname)}`;
+        cb(null, fileName);
+    }
+});
+
+let upload = multer({ storage });
+
+router.get('/login', controlador.login);
+router.get('/register', controlador.register);
+
+module.exports = router;
