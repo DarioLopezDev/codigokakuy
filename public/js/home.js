2aa2f2cc77adc4918410e166f9f2739fff3b11e2
AntuzAdolfo <antumaxis@gmail.com>
2024-04-08 07:00:51 -0300

    Terminando los Carruseles

--- public/js/home.js
+++ public/js/home.js
@@ -1,50 +1,103 @@
 window.addEventListener('load', () => {
-    //Traigo el contenedor de imágenes y c/u de los puntos
+    //Traigo el contenedor de imágenes y los puntos
     const booksContainer = document.querySelector('.books-container');
     const left = document.querySelector('.left');
+    let count = 1;
     const right = document.querySelector('.right');
-    /*
-    const allPoint = document.querySelectorAll('.dot');
-
-    //Recorro c/u de los puntos
-    allPoint.forEach((punto, index) => {
-        //Le agrego el evento click
-        allPoint[index].addEventListener('click', () => {
-            //Guardo la posición
-            let position = index;
-            //Calculo el valor de desplazamiento X que tendrá
-            let traslateXValue = position * -20;
-            //Le cambio el valor de transform correspondiente
-            imgContainer.style.transform = `translateX(${traslateXValue}%)`; 
-
-          //Cambio de color del punto seleccionado
-            //Elimino la clase 'active' de todos los puntos
-            allPoint.forEach((punto, index) => {
-                allPoint[index].classList.remove('active');
-            });
-            //Agrego la clase 'active' al punto actual
-            allPoint[index].classList.add('active');
-
-        });
-    });   
-    */
-
-    //Consulto el valor de la traslación en X del contenedor de libros
-    let elementStyle = window.getComputedStyle(booksContainer);
-    console.log(elementStyle);
-    let translateValue = elementStyle.getPropertyValue('transform');
-    console.log(translateValue);
-    
 
     left.addEventListener('click', () => {
-        //Consulto el valor de la traslación en X del contenedor de libros
-        let elementStyle = window.getComputedStyle(booksContainer);
-        let translateValue = elementStyle.getPropertyValue('transform');
-        console.log(translateValue);
-        //Calculo el valor de desplazamiento X que tendrá
-        let traslateXValue = position * 20;
+        count++;
         //Le cambio el valor de transform correspondiente
-        booksContainer.style.transform = `translateX(${traslateXValue}%)`;
+        booksContainer.style.transform = `matrix(1, 0, 0, 1, ${count * 150}, 0)`;
+    });
+    
+    right.addEventListener('click', () => {
+        count--;
+        if (count === 0) {
+            count = -1;            
+        }
+        //Le cambio el valor de transform correspondiente
+        booksContainer.style.transform = `matrix(1, 0, 0, 1, ${count * 150}, 0)`;
     });
 
+
+    const actionBooksContainer = document.querySelector('.action-books-container');
+    const leftAction = document.querySelector('.left-action');
+    let countAction = 1;
+    const rightAction = document.querySelector('.right-action');
+
+    leftAction.addEventListener('click', () => {
+        countAction++;
+        //Le cambio el valor de transform correspondiente
+        actionBooksContainer.style.transform = `matrix(1, 0, 0, 1, ${countAction * 150}, 0)`;
+    });
+    
+    rightAction.addEventListener('click', () => {
+        countAction--;
+        if (countAction === 0) {
+            countAction = -1;            
+        }
+        //Le cambio el valor de transform correspondiente
+        actionBooksContainer.style.transform = `matrix(1, 0, 0, 1, ${countAction * 150}, 0)`;
+    });
+
+    const scifiBooksContainer = document.querySelector('.scifi-books-container');
+    const leftScifi = document.querySelector('.left-scifi');
+    let countScifi = 1;
+    const rightScifi = document.querySelector('.right-scifi');
+
+    leftScifi.addEventListener('click', () => {
+        countScifi++;
+        //Le cambio el valor de transform correspondiente
+        scifiBooksContainer.style.transform = `matrix(1, 0, 0, 1, ${countScifi * 150}, 0)`;
+    });
+    
+    rightScifi.addEventListener('click', () => {
+        countScifi--;
+        if (countScifi === 0) {
+            countScifi = -1;            
+        }
+        //Le cambio el valor de transform correspondiente
+        scifiBooksContainer.style.transform = `matrix(1, 0, 0, 1, ${countScifi * 150}, 0)`;
+    });
+    
+    const dramaBooksContainer = document.querySelector('.drama-books-container');
+    const leftDrama = document.querySelector('.left-drama');
+    let countDrama = 1;
+    const rightDrama = document.querySelector('.right-drama');
+
+    leftDrama.addEventListener('click', () => {
+        countDrama++;
+        //Le cambio el valor de transform correspondiente
+        dramaBooksContainer.style.transform = `matrix(1, 0, 0, 1, ${countDrama * 150}, 0)`;
+    });
+    
+    rightDrama.addEventListener('click', () => {
+        countDrama--;
+        if (countDrama === 0) {
+            countDrama = -1;            
+        }
+        //Le cambio el valor de transform correspondiente
+        dramaBooksContainer.style.transform = `matrix(1, 0, 0, 1, ${countDrama * 150}, 0)`;
+    });
+
+    const economyBooksContainer = document.querySelector('.economy-books-container');
+    const leftEconomy = document.querySelector('.left-economy');
+    let countEconomy = 1;
+    const rightEconomy = document.querySelector('.right-economy');
+
+    leftEconomy.addEventListener('click', () => {
+        countEconomy++;
+        //Le cambio el valor de transform correspondiente
+        economyBooksContainer.style.transform = `matrix(1, 0, 0, 1, ${countEconomy * 150}, 0)`;
+    });
+    
+    rightEconomy.addEventListener('click', () => {
+        countEconomy--;
+        if (countEconomy === 0) {
+            countEconomy = -1;            
+        }
+        //Le cambio el valor de transform correspondiente
+        economyBooksContainer.style.transform = `matrix(1, 0, 0, 1, ${countEconomy * 150}, 0)`;
+    });
 }); 
--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -23,8 +23,59 @@ const controlador = {
                 books[books.length - 4].dataValues,
                 books[books.length - 5].dataValues
             ];
+
+            const actionBooks = await db.Book.findAll({
+                include: [
+                    { association: 'author' },
+                    { association: 'genre' },
+                    { association: 'publisher' }
+                ],
+                where: {
+                    genre_id: 1
+                }
+            });
+
+            const scifiBooks = await db.Book.findAll({
+                include: [
+                    { association: 'author' },
+                    { association: 'genre' },
+                    { association: 'publisher' }
+                ],
+                where: {
+                    genre_id: 4
+                }
+            });
+
+            const dramaBooks = await db.Book.findAll({
+                include: [
+                    { association: 'author' },
+                    { association: 'genre' },
+                    { association: 'publisher' }
+                ],
+                where: {
+                    genre_id: 3
+                }
+            });
+
+            const economyBooks = await db.Book.findAll({
+                include: [
+                    { association: 'author' },
+                    { association: 'genre' },
+                    { association: 'publisher' }
+                ],
+                where: {
+                    genre_id: 5
+                }
+            });
         
-            res.render('index', {books, lastBooks});
+            res.render('index', {
+                books, 
+                lastBooks, 
+                actionBooks,
+                scifiBooks,
+                dramaBooks,
+                economyBooks
+            });
 
         } catch (error) {
             console.log(error.message); 
